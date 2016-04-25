1da5985f2044b28405c33e8055f72c7a04d8e99d
Antonio Fernandez <eric6822@gmail.com>
2016-04-25 21:06:42 +0200

    exercise done

---
+++ caesar_cypher.js
@@ -0,0 +1,52 @@
+"use strict";
+var caesar = function caesarCipher(message, shift) {
+  var result_numbers = [];
+  var result = [];
+
+  if (shift == undefined){
+  	shift = -3;
+  }
+
+  for(var i = 0 ; i < message.length; i++){
+  		if((message.charAt(i) >= "a" && message.charAt(i) <= "z") || (message.charAt(i) >= "A" && message.charAt(i) <= "Z")){
+  		result_numbers[i] = message.charCodeAt(i) + (shift);
+    	} else{
+    	result_numbers[i] = message.charCodeAt(i);	
+    	}
+  }
+
+  result_numbers.forEach (function(number){
+
+  		result.push(String.fromCharCode(number));
+  });
+
+  return result.join("");
+};
+
+function caesarBuilder(message, shift) {
+     return caesar(message,shift);
+}
+
+class WholeCipher{
+
+	constructor(message, shift1){
+		this.message = message;
+		this.shift1 = shift1;
+	}
+	
+}
+
+
+
+//var encrypted = caesarCipher("brutus");
+//console.log(encrypted);
+//=> "_orqrp"
+
+//encrypted = caesarCipher("Et tu, brute?",6);
+//console.log(encrypted);
+//=> "Bq qr, _orqb?"
+
+var encrypted = caesarBuilder("Et tu, brute?", 6);
+console.log(encrypted);
+//=> "Kz z{, hx{zk?"
+//=> "Kz za, hxazk?"
